d4a2be6d554cf49eaffd2096e377d5ea9678fe41
upadhb <bishal.u87@gmail.com>
2017-12-11 18:26:45 -0600

    adding redux essentials for the app

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -1,14 +1,78 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as courseActions from '../../actions/courseActions';
 
 class CoursePage extends React.Component {
+    constructor (props, context) {
+        super(props, context);
+
+        this.state = {
+            course: { title: '' }
+        };
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+
+    onClickSave(){
+        this.props.actions.createCourse(this.state.course);
+    }
+
+    courseRow(course, index) {
+        return (
+            <div key={index} className='col-md-12'>
+                {index+1+')'} {course.title}
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div className='jumbotron'>
-                <h1>Course Page</h1>
+            <div className='jumbotron row'>
+                <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
+                <div className='col-md-12'>
+                    <hr/>
+                </div>
+                <div className='col-md-1'>
+                    <label htmlFor='courseName'>Add Course</label>
+                </div>
+                <div className='col-md-3'>
+                    <input id='courseName' type='text'
+                           className='form-control'
+                           onChange={this.onTitleChange}
+                           value={this.state.course.title}/>
+                </div>
+                <div className='col-md-1'>
+                    <button className='btn btn-success' onClick={this.onClickSave}>Save</button>
+                </div>
             </div>
         );
     }
 }
 
-export default CoursePage;
+CoursePage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps (state, ownProps) {
+    return {
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursePage);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router'
-import routes from './routes';
+import { Provider } from 'react-redux';
 import './styles/styles.css';   // We can use Webpack to import CSS files!!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+import configureStore from './store/configureStore';
+import routes from './routes';
+
+const store = configureStore();
 render (
-  <Router history={browserHistory} routes={routes} />,
+    <Provider store = {store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
